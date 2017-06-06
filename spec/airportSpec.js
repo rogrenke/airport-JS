d3fe5ae7ea79caf8c63b542c66b933a2b6d78d5f
“rogrenke” <roland@rolandgrenke.com>
2017-06-06 16:48:25 +0100

    Creates new Plane object, adds functionality to land a plane add an object

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,12 +1,19 @@
 describe("Airport", function() {
 
 	var airport;
+	var plane;
 
 	beforeEach(function() {
 		airport = new Airport();
+		plane = new Plane();
 	});
 
-	it("It is created as an object with an empty planes array", function() {
+	it("is created as an object with an empty planes array", function() {
 		expect(airport.planes).toEqual([]);
+	})
+
+	it("is expected to store a plane if the land function is called on it with plane as an argument", function() {
+		airport.land(plane);
+		expect(airport.planes[0]).toEqual(plane);
 	});
 });
--- src/airport.js
+++ src/airport.js
@@ -2,7 +2,12 @@ function Airport() {
 	this.planes = []
 }
 
+Airport.prototype.land = function(plane) {
+	this.planes.push(plane);
+}
 
+function Plane() {
+}
 
 
 // function Airport() {}
